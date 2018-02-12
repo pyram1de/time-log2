505d2ad381edb818740690af1a77c9350acbf7f7
David Hulmes <dhulmes@gmail.com>
2018-02-12 21:54:52 +0000

    create html teamplate

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,22 +2,20 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    template: `
-    <div>
-        <h1>Upcoming Angular 2 Events </h1>
-        <hr>
-        <h2>{{event.name}}</h2>
-        <hr/>
-    </div>
-    `
+    templateUrl: 'events-list.component.html'
 })
 export class EventsListComponent {
     event = {
         id: 1,
-        name: 'name',
+        name: 'Angular Connect',
         date: '9/9/2020',
         time: '10:00',
         price: 100.00,
-        imageUrl: '/app/assets/images/angularconnect-shield.png'
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1075 DY',
+            city: 'London',
+            country: 'England'
+        }
     }
 }
