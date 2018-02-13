aeca2292d023d5b5fd0221298f5114e34d1e030a
David Hulmes <David.Hulmes@airsweb.com>
2018-02-13 14:24:50 +0000

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {EventsAppComponent} from './events-app.component'
 import {EventsListComponent} from './events/events-list.component'
+import {EventThumbnailComponent} from './events/event-thumbnail.component'
+import {NavBarComponent} from './nav/navbar.component'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent,
-        EventsListComponent],
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavBarComponent],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
