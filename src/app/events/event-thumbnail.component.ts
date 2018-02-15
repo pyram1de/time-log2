641058270ccc152fe59e1a7ef5c1921ef74c37bd
David Hulmes <David.Hulmes@airsweb.com>
2018-02-15 13:59:50 +0000

    bindings etc ngfor ngif

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,19 +6,40 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
     <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
+        <div [ngClass]="getStartTimeClass()" [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">Time: {{event.time}}
+            <span *ngSwitchCase="'8:00 am'" >(early start)</span>
+            <span [style.color]="event.time === '10:00 am' ? '#ff0000' : '#bbb'" *ngSwitchCase="'10:00 am'">(late start)</span>
+            <span [ngStyle]="getStartTimeStyle()" *ngSwitchDefault>(normal start)</span>
+        </div>
         <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}} </span>
+        <div *ngIf="event.location">
+            <span>Location: {{event.location?.address}}</span>
+            <span class="pad-left">{{event.location?.city}}, {{event.location?.country}} </span>
+        </div>
+        <div *ngIf="event.onlineUrl">
+            Online Url: {{event.onlineUrl}}
         </div>
     </div>
     `,
     styles: [`
+        .green { color: #4ead4e !Important;}
+        .bold { font-weight: bold;}
         .pad-left { margin-left: 10px; }
+        .thumbnail { min-height: 210px; }
     `]
 })
 export class EventThumbnailComponent {
     @Input() event: any
 
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event && this.event.time === '8:00 am';
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
+    getStartTimeStyle(): any {
+        const isEarlyStart = this.event && this.event && this.event.time === '9:00 am';
+        if(isEarlyStart){
+            return {color: '#ff33ff', 'font-weight': 'bold'}
+        }
+        return {};
+    }
 }
