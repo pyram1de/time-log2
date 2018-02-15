1522aab11b3828171403fecccbcbc86d200ad7c4
David Hulmes <dhulmes@gmail.com>
2018-02-15 21:51:05 +0000

    services

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,20 @@ import {EventsAppComponent} from './events-app.component'
 import {EventsListComponent} from './events/events-list.component'
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
+import {EventService} from './events/shared/event.service'
+import {ToastrService} from './common/toastr.service'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
-        NavBarComponent],
-    bootstrap: [EventsAppComponent]
+        NavBarComponent
+    ],
+    bootstrap: [EventsAppComponent],
+    providers: [
+        EventService,
+        ToastrService
+    ]
 })
 export class AppModule {}
