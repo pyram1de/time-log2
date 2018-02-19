ffa50a04208bc78dd98169b8225b537a5b46c58e
David Hulmes <David.Hulmes@airsweb.com>
2018-02-19 13:28:43 +0000

    navigating though pages

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,9 +7,15 @@ import {NavBarComponent} from './nav/navbar.component'
 import {EventService} from './events/shared/event.service'
 import {ToastrService} from './common/toastr.service'
 import {EventDetailsComponent} from './events/event-details/event-details.component'
+import {appRoutes} from './routes'
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
