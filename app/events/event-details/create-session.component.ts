cfc933bdab819b93d54486b99ca11f3b65705314
David Hulmes <David.Hulmes@airsweb.com>
2018-03-06 13:57:14 +0000

    creating custom validator

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { ISession } from '../shared/index'
+import { ISession, restrictedWords } from '../shared/index'
 @Component({
     templateUrl: 'create-session.component.html',
     styles: [`
@@ -25,7 +25,7 @@ export class CreateSessionComponent  implements OnInit {
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('',[Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('',[Validators.required, Validators.maxLength(400), restrictedWords(['foo','bar'])]);
         
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -36,6 +36,8 @@ export class CreateSessionComponent  implements OnInit {
         })
 
     }
+
+
 
     saveSession(formValues){
         
